4ecffc7b317a8f298f20ce8184ce8728be120d4f
Utkarsh Ajay Patil <utkarshp2305@gmail.com>
2017-03-07 00:50:52 +0530

    Carousel with placeholder images

--- js/home.js
+++ js/home.js
@@ -1,4 +1,10 @@
-$(function() {
-  $('#header').load('header.html');
-  $('#footer').load('footer.html');
+function autoplay() {
+	$('.carousel').carousel('next');
+	setTimeout(autoplay, 3500);
+};
+$(document).ready(function() {
+	$('#header').load('header.html');
+	$('#footer').load('footer.html');
+	$('.carousel.carousel-slider').carousel({fullWidth: true});
+	setTimeout(autoplay, 2000);
 });
