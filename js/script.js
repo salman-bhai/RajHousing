60ca26981b24b1acb74ef405e58cc5f308353a0c
Utkarsh Ajay Patil <utkarshp2305@gmail.com>
2017-03-30 01:54:18 +0530

    Update team page with actual data

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ function init() {
 		$('#header').load('header.html');
 		$('#footer').load('footer.html');
 		$('.materialboxed').materialbox();
-		$('.slider').slider({interval: 3000, height: 550});
+		$('.slider').slider({interval: 3000, height: 570});
 	});
 }
 
-function get_model(member){
+function get_web_model(member){
 	var model = "";
 	model += '<div class="team-member"><div class="card center-align">';
 	model += '<div class="card-content">';
@@ -25,21 +25,36 @@ function get_model(member){
 	return model;
 }
 
+function get_managing_model(member){
+	var model = "";
+	model += '<div class="team-member"><div class="card center-align">';
+	model += '<div class="card-content">';
+	model += '<div class="team-member-bg indigo darken-4"></div>';
+	model += '<img src="'+member.image+'" class="circle responsive-img materialboxed" data-caption="'+member.name+'">';
+	model += '<h6><b>'+member.name+'</b></h6>';
+	model += '<div class="team-member-info">'
+	model += '<br/>'+member.email+'<br/>';
+	model += member.mobile+'<br/>';
+	model += '<div class="row">';
+	model += '<a href="'+member.linkedin+'" rel="nofollow"><img src="images/social_media_logos/logo_linkedin.png" id="social-media" alt="linkedin logo"></a>';
+	model += '<a href="'+member.facebook+'" rel="nofollow"><img src="images/social_media_logos/logo_fb.png" id="social-media" alt="fb logo"></a>'
+	model += '</div></div></div></div>';
+	return model;
+}
+
 function load_members(){
 	var gallery_container = document.getElementById("gallery");
 	// Core members
 	var gallery_row;
-	gallery_container.innerHTML += '<h3 class="label">Core Team</h3>';
+	gallery_container.innerHTML += '<h3 class="label">Managing Director</h3>';
 	gallery_container.innerHTML += '<div id="core-gallery-row"></div>';
 	gallery_row = document.getElementById('core-gallery-row');
 	for(var i=0;i<core_members.length;i++){
 	  var member = core_members[i];
-	  gallery_row.innerHTML += '<div class="gallery" id="core-member-'+i+'"></div>';
+	  gallery_row.innerHTML += '<div class="gallery" style="display: table; margin: 0 auto;" id="core-member-'+i+'"></div>';
 	  var member_container = document.getElementById('core-member-'+i);
-	  member_container.innerHTML += get_model(member);
+	  member_container.innerHTML += get_managing_model(member);
 	}
-	gallery_container.innerHTML += '<div class="center button-container"><a class="prev-core btn-floating btn-large waves-effect waves-light indigo darken-4"><i class="material-icons">chevron_left</i></a>';
-	gallery_container.innerHTML += '<a class="next-core btn-floating btn-large waves-effect waves-light indigo darken-4"><i class="material-icons">chevron_right</i></a></div>';
 	// Tech Members
 	gallery_container.innerHTML += '<h3 class="label">Website Developer Team</h3>';
 	gallery_container.innerHTML += '<div  id="tech-gallery-row"></div>';
@@ -48,8 +63,10 @@ function load_members(){
 	  var member = tech_members[i];
 	  gallery_row.innerHTML += '<div class="gallery" id="tech-member-'+i+'"></div>';
 	  var member_container = document.getElementById('tech-member-'+i);
-	  member_container.innerHTML += get_model(member);
+	  member_container.innerHTML += get_web_model(member);
 	}
+	gallery_container.innerHTML += '<div class="center button-container"><a class="prev-core btn-floating btn-large waves-effect waves-light indigo darken-4"><i class="material-icons">chevron_left</i></a>';
+	gallery_container.innerHTML += '<a class="next-core btn-floating btn-large waves-effect waves-light indigo darken-4"><i class="material-icons">chevron_right</i></a></div>';
 }
 
 function initializeTeamMembers() {
@@ -159,9 +176,60 @@ function goToPreviousMember() {
 }
 
 function goToNextCore() {
-	$('#core-gallery-row').animate({scrollLeft: $('#core-gallery-row').scrollLeft() + $('.team-member').width() + 10}, 500);
+	$('#tech-gallery-row').animate({scrollLeft: $('#tech-gallery-row').scrollLeft() + $('.team-member').width() + 10}, 500);
 }
 
 function goToPrevCore() {
-	$('#core-gallery-row').animate({scrollLeft: ($('#core-gallery-row').scrollLeft() - $('.team-member').width() - 10)}, 500);
+	$('#tech-gallery-row').animate({scrollLeft: ($('#tech-gallery-row').scrollLeft() - $('.team-member').width() - 10)}, 500);
 }
+
+var core_members = [
+    {
+        "name": "Naveen Bansal",
+        "mobile": "+91",
+        "email": "@.com",
+        "facebook": "https://www.facebook.com/navin.bansal.9484",
+        "linkedin":  "https://in.linkedin.com/",
+        "image": "images/team/team_naveen.jpg"
+    }
+]
+
+var tech_members = [
+	{
+        "name": "Salman Shah",
+        "position": "Web and App Developer",
+        "mobile": "+91 88618 97183",
+        "email": "sbs.191197@gmail.com",
+        "facebook": "https://www.facebook.com/salman.shah.92775",
+        "linkedin": "https://www.linkedin.com/in/sbshah97",
+        "image": "images/team/team_salman.jpg"
+    },
+    {
+        "name": "Hrishi Hiraskar",
+        "position": "Web and App Developer",
+        "mobile": "+91 8147298166",
+        "email": "hrishihiraskar@gmail.com",
+        "facebook": "https://www.facebook.com/hrishi.hiraskar7",
+        "linkedin":  "https://www.linkedin.com/in/hrishi-hiraskar",
+        "image": "images/team/team_hrishi.jpg"
+    },
+    {
+        "name": "Utkarsh Patil",
+        "position": "Web Intern",
+        "mobile": "+91 90087 91852",
+        "email": "utkarshp2305@gmail.com",
+        "facebook": "https://www.facebook.com/utkarsh.patil.9659",
+        "linkedin": "http://www.linkedin.com/",
+        "image": "images/team/team_utkarsh.png"
+    },
+    {
+        "name": "Sunitha Selvan",
+        "position": "Web Intern",
+        "mobile": "+91 ",
+        "email": "@.com",
+        "facebook": "https://www.facebook.com/selvan.suni",
+        "linkedin":  "https://in.linkedin.com/",
+        "image": "images/team/team_sunitha.jpg"
+
+    }
+]
