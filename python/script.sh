2752ee90f7f87a55782112cae2af46a0a3aa48c4
salman-bhai <sbs.191197@gmail.com>
2017-05-14 12:46:47 +0530

    IMages re-sized

---
+++ python/script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python2 image_compress.py --mode compress ../images/careers/.
+python2 image_compress.py --mode compress ../images/carousel/.
+python2 image_compress.py --mode compress ../images/team/.
+python2 image_compress.py --mode compress ../images/.
+python2 image_compress.py --mode compress ../images/social_media_logos/.
